7e249dfafcafd0f98aa9fc52646de4037337c96c
Ben Pearce <ben.pearce@octopus.com>
2017-11-21 15:48:20 +1000

    Fixed namespace

--- source/Client/Configuration/UsernamePasswordConfigurationResource.cs
+++ source/Client/Configuration/UsernamePasswordConfigurationResource.cs
@@ -1,6 +1,6 @@
 ﻿using Octopus.Client.Extensibility.Extensions.Infrastructure.Configuration;
 
-namespace Octopus.Server.Extensibility.Authentication.UsernamePassword.Configuration
+namespace Octopus.Client.Extensibility.Authentication.UsernamePassword.Configuration
 {
     public class UsernamePasswordConfigurationResource : ExtensionConfigurationResource
     {
