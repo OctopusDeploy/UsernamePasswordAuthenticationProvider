159ad16a4eeb60253dfe32e5df4cac890eab0de7
johnsimons <john.simons@octopus.com>
2018-10-04 17:55:16 +1000

    Removing Nancy

--- source/Server/Configuration/DatabaseInitializer.cs
+++ source/Server/Configuration/DatabaseInitializer.cs
@@ -38,7 +38,7 @@ namespace Octopus.Server.Extensibility.Authentication.UsernamePassword.Configura
             var authenticationMode = settings.Get("Octopus.WebPortal.AuthenticationMode", string.Empty);
             doc = new UsernamePasswordConfiguration("UsernamePassword", "Octopus Deploy")
             {
-                IsEnabled = legacyModes.Any(x => x.Equals(authenticationMode.Replace("\"", ""), StringComparison.InvariantCultureIgnoreCase)),
+                IsEnabled = legacyModes.Any(x => x.Equals(authenticationMode.Replace("\"", ""), StringComparison.OrdinalIgnoreCase)),
             };
 
             configurationStore.Create(doc);
